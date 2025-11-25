53b581c701eea77863ecbf55f8d192b6350abaef
Kenneth Pamintuan <parchedAlbumen>
2025-11-24 20:49:16 -0800

    WE ARE GETTING SOMEWHERE WITH THE MOVEMENTS

--- rubikscube/Movements.java
+++ rubikscube/Movements.java
@@ -4,6 +4,23 @@ public class Movements {
     public static void doMoves(Cubie cubie, char movement) {
         //FURDLB | do magic here
         if (movement == 'F') {
+            //corner permutation part 
+            int tempOrieP = cubie.getCornerP(0); //DLF becomes temp
+            cubie.setCornerP(0, cubie.getCornerP(1)); //URF becomes UFL (clockwise motion)
+            cubie.setCornerP(1, cubie.getCornerP(5)); //UFL -> DLF
+            cubie.setCornerP(5, cubie.getCornerP(4)); //DLF -> DFR
+            cubie.setCornerP(4, tempOrieP); //DFR -> temp
+
+            //corner orientation part (idk how to do this)
+
+            //edge permutation part  UF->FR->DF->FL->UF
+            int tempEdgeP = cubie.getEdgeP(1); //UF
+            cubie.setEdgeP(1, cubie.getEdgeP(9)); //UF -> FL
+            cubie.setEdgeP(9, cubie.getEdgeP(5));
+            cubie.setEdgeP(5, cubie.getEdgeP(8));
+            cubie.setEdgeP(8, tempEdgeP);
+
+            //edge orientation part 
             //do magic 
         } else if (movement == 'U') {
             //do magic
